0cfb214ce89b6e1316926b08d51e3cc705f4fed0
CeeDeeBee <cdbarnes98@gmail.com>
2018-08-29 17:44:29 -0400

    Implemented Obstacles and Collision

--- game.js
+++ game.js
@@ -1,14 +1,24 @@
 //Code for the game portion of the page
 //On DOM load
 $(document).ready(function() {
+	var jump = false;
+	var started = false;
+	var shouldLoop = true;
 	var $div = $("<div>", {id: "testDiv", "class": "div"}).html("Test");
 	$("body").append($div);
 	//User input event listener
 	$(document).on("keydown", function(e) {
-		console.log(e);
 		if (e.key == 'ArrowUp') {
-			jump();
+			jump = true;
+			if (!started) {
+				animate();
+				started = true;
+			}
 		}
+	});
+	$("#newFrame").click(function() {
+		shouldLoop = false;
+		animate();
 	});
 	//Code for canvas
 	var canvas = document.getElementById("canvas");
@@ -17,49 +27,60 @@ $(document).ready(function() {
 	//set default values
 	var fps = 60;
 	var percent = 100;
-	var direction = 1;
+	var direction = 5;
 	var frame = 0;
+	var obstacles = [false, false, false, false, false, false, false, false, false, false,
+					 false, false, false, false, false, false, false, false, false, false,
+					 false, false, false, false, false, false, false, false, false, false,
+					 false, false, false, false, false, false, false, false, false, true
+					];
 
-	function jump() {
-		if (frame == 50) {
-			percent = 100;
-			frame = 0;
-		}
-		percent += direction;
-		if (percent < 0) {
-			percent = 0;
-			direction = 1;
-		} else if (percent > 25) {
-			percent = 25;
-			direction = -1;
+	//animate();
+	drawStage();
+	drawRect({x: 100, y: 160});
+
+	function animate() {
+		//console.log(obstacles);
+		if (jump == true) {
+			if (frame == 42) {
+				percent = 100;
+				frame = 0;
+				jump = false;
+			}
+			percent += direction;
+			if (percent < 0) {
+				percent = 0;
+				direction = 5;
+			} else if (percent > 100) {
+				percent = 100;
+				direction = -5;
+			}
+			frame += 1;
 		}
 
 		draw(percent);
 
-		frame += 1;
-		if (frame < 50) {
+		if (shouldLoop) {
 			setTimeout(function() {
-				requestAnimationFrame(jump);
+				requestAnimationFrame(animate);
 			}, 1000 / fps);
 		}
+		//console.log(frame);
 	}
 
 	function draw(sliderValue) {
-		//clear canvas
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.lineWidth = 5;
-
-		
+		drawStage();
+		/*
 		ctx.beginPath();
 		ctx.moveTo(100, 20);
 		ctx.lineTo(100, 160);
 		ctx.strokeStyle = "black";
 		ctx.stroke();
-		
+		*/
 
 		var xy;
 
-		var percent = sliderValue / 25;
+		var percent = sliderValue / 100;
 		xy = getLineXYatPercent({
 			x: 100,
 			y: 20
@@ -67,7 +88,15 @@ $(document).ready(function() {
 			x: 100,
 			y: 160
 		}, percent);
+
 		drawRect(xy);
+
+		if ((xy.y - 8) >= 140) {
+			if (obstacles[1] == true || obstacles[2] == true || obstacles[3] == true || obstacles[4] == true) {
+				console.log("collision");
+				shouldLoop = false;			
+			}
+		}
 	}
 
 	function getLineXYatPercent(startPt, endPt, percent) {
@@ -82,6 +111,7 @@ $(document).ready(function() {
 	}
 
 	function drawRect(point) {
+		//console.log(point);
 		ctx.fillStyle = "white";
 		ctx.strokeStyle = "green";
 		ctx.lineWidth = 3;
@@ -89,5 +119,46 @@ $(document).ready(function() {
 		ctx.rect(point.x - 13, point.y - 8, 25, 15);
 		ctx.fill();
 		ctx.stroke();
+	}
+
+	function drawStage() {
+		//clear canvas
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.lineWidth = 3;
+
+		//draw platform
+		ctx.beginPath();
+		ctx.moveTo(50, 170);
+		ctx.lineTo(550, 170);
+		ctx.strokeStyle = "black";
+		ctx.stroke();
+
+		$.each(obstacles, function(index, value) {
+			var point = 0;
+			if (value) {
+				point = (index * 12.5) + 50;
+				drawObstacle(point);
+				obstacles[index] = false;
+				if (index != 0) {
+					obstacles[index - 1] = true;
+				}
+			}
+		});
+
+		if (Math.random() < .05) {
+			obstacles[39] = true;
+		}
+	}
+
+	function drawObstacle(point) {
+		//draw obstacles
+		ctx.fillStyle = "black";
+		ctx.strokeStyle = "black";
+		ctx.lineWidth = 3;
+		ctx.beginPath();
+		ctx.rect(point, 155, 15, 15);
+		ctx.fill();
+		ctx.stroke();
+
 	}
 });
